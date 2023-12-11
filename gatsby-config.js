2d093bf4d33e79a8dec0a9281bda457ab765c4c3
salemaziel <mymainemail0501@gmail.com>
2023-12-11 15:03:50 -0800

    Update Matomo and Google Analytics configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,10 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+ })
+
 const path = require('path')
 const config = require("./config/site")
 
@@ -41,8 +45,8 @@ module.exports = {
       resolve: "gatsby-plugin-matomo",
       options: {
         siteId: "2",
-        matomoUrl: "https://analytics.viadelweb.cloud/",
-        siteUrl: "https://viadelweb.com/",
+        matomoUrl: "https://analytics.viadelweb.cloud",
+        siteUrl: "https://viadelweb.com",
         // All the optional settings
 //        matomoPhpScript: "piwik.php",
         matomoJsScript: "matomo.js",
@@ -53,6 +57,38 @@ module.exports = {
 //        localScript: "/piwik.js",
         dev: false,
         enableJSErrorTracking: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "process.env.GA_UA_ID", // Google Analytics / GA
+          "process.env.GA_ID", // Google Analytics / GTag
+          //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+          origin: "YOUR_SELF_HOSTED_ORIGIN",
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
       },
     },
 //    {
@@ -95,4 +131,5 @@ module.exports = {
       },
     },
   ],
+  partytownProxiedURLs: [`https://www.googletagmanager.com/gtag/js?id=${process.env.GATSBY_GA_ID}`]
 }
