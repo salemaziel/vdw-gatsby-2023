fcf43f7c01909fb9c324bae821a77e6c965460dc
salemaziel <mymainemail0501@gmail.com>
2023-12-29 17:52:14 -0800

    Fix Google Analytics tracking ID in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
       options: {
         //You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          //"process.env.GA_UA_ID", // Google Analytics / GA
+          "process.env.GA_UA_ID", // Google Analytics / GA
           "process.env.GA_MEASUREMENT_ID", // Google Analytics / GTag
           //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
           //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
