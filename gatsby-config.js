f3012b45e82f478aab880d89bf868d00a4a613bc
salemaziel <mymainemail0501@gmail.com>
2023-12-29 17:23:02 -0800

    Add gatsby-plugin-google-gtag to package.json and yarn.lock

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,9 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
-//require("dotenv").config({
-//  path: `.env.${process.env.NODE_ENV}`,
-// })
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+ })
 
 const path = require('path')
 const config = require("./config/site")
@@ -61,38 +61,38 @@ module.exports = {
 
       },
     },
-    //{
-      //resolve: `gatsby-plugin-google-gtag`,
-      //options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        //trackingIds: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        //You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
           //"process.env.GA_UA_ID", // Google Analytics / GA
-          //"process.env.GA_ID", // Google Analytics / GTag
+          "process.env.GA_MEASUREMENT_ID", // Google Analytics / GTag
           //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
           //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-        //],
+        ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
-        //gtagConfig: {
+        gtagConfig: {
           //optimize_id: "OPT_CONTAINER_ID",
-          //anonymize_ip: true,
-          //cookie_expires: 0,
-        //},
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
         // This object is used for configuration specific to this plugin
-        //pluginConfig: {
+        pluginConfig: {
           // Puts tracking script in the head instead of the body
-          //head: false,
+          head: false,
           // Setting this parameter is also optional
-          //respectDNT: true,
+          respectDNT: true,
           // Avoids sending pageview hits from custom paths
-          //exclude: ["/preview/**", "/do-not-track/me/too/"],
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
           // Defaults to https://www.googletagmanager.com
           //origin: "YOUR_SELF_HOSTED_ORIGIN",
           // Delays processing pageview events on route update (in milliseconds)
-          //delayOnRouteUpdate: 0,
-        //},
-     // },
-    //},
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
 //    {
 //      resolve: `gatsby-source-filesystem`,
 //        options: {
